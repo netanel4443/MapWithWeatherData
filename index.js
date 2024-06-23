7b9a4ddc625e73191b1f4e84109a460c8147bca8
Netanel <netanel4443@gmail.com>
2024-06-23 21:34:32 +0300

    add navigation packages & logic to the app

--- index.js
+++ index.js
@@ -1,17 +1,31 @@
-/**
- * @format
- */
-
 import {AppRegistry} from 'react-native';
 import MapScreen from './src/mapscreen/ui/MapScreen';
 import {name as appName} from './app.json';
 import {Provider} from 'react-redux';
-import {store} from './src/redux/store'
+import {store} from './src/redux/store';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import SavedDataScreen from './src/saveddatascreen/SavedDataScreen';
+
+const Stack = createNativeStackNavigator();
 
 const app = () => {
   return (
     <Provider store={store}>
-      <MapScreen />
+      <NavigationContainer>
+        <Stack.Navigator initialRouteName="MapScreen">
+          <Stack.Screen
+            name="MapScreen"
+            component={MapScreen}
+            options={{headerShown: false}}
+          />
+          <Stack.Screen
+            name="MarkersData"
+            component={SavedDataScreen}
+            options={{title: 'Markers Data'}}
+          />
+        </Stack.Navigator>
+      </NavigationContainer>
     </Provider>
   );
 };
